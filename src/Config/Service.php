b0b3b0df0dc2f976b4d4fbb21de623130afa29c8
Gauthier Delamarre <gauthier@objective-php.org>
2016-01-07 15:28:37 +0100

    Add support for new configuration mechanism

---
+++ src/Config/Service.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * This file is part of the Objective PHP project
+     *
+     * More info about Objective PHP on www.objective-php.org
+     *
+     * @license http://opensource.org/licenses/GPL-3.0 GNU GPL License 3.0
+     */
+    
+    namespace ObjectivePHP\ServicesFactory\Config;
+    
+    use ObjectivePHP\Config\StackedValuesDirective;
+
+    class Service extends StackedValuesDirective
+    {
+        const DIRECTIVE = 'services';
+    }
