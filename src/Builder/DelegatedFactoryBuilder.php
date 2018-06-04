8b353fdad4f8d0284c069235f0b1bb6bcd9c38d4
Vincent Simonin <vsi@opcoding.eu>
2018-06-04 13:26:47 +0200

    Fix parameter processing in delegated factory builder

--- src/Builder/DelegatedFactoryBuilder.php
+++ src/Builder/DelegatedFactoryBuilder.php
@@ -9,14 +9,12 @@ use ObjectivePHP\ServicesFactory\Specification\ServiceSpecificationInterface;
 
 class DelegatedFactoryBuilder extends AbstractServiceBuilder
 {
-
     /**
      * Service definition types this builder can handle
      *
      * @var array
      */
     protected $handledSpecs = [DelegatedFactorySpecification::class];
-
 
     /**
      * @param DelegatedFactorySpecification $serviceSpecification
@@ -27,29 +25,31 @@ class DelegatedFactoryBuilder extends AbstractServiceBuilder
      */
     public function build(ServiceSpecificationInterface $serviceSpecification, $params = [], string $serviceId = null)
     {
-
         // check compatibility with the service definition
         if (!$this->doesHandle($serviceSpecification)) {
-            throw new ServicesFactoryException(sprintf('"%s" service definition is not handled by this builder.',
-                get_class($serviceSpecification)), ServicesFactoryException::INCOMPATIBLE_SERVICE_DEFINITION);
+            throw new ServicesFactoryException(
+                sprintf(
+                    '"%s" service definition is not handled by this builder.',
+                    get_class($serviceSpecification)
+                ),
+                ServicesFactoryException::INCOMPATIBLE_SERVICE_DEFINITION
+            );
         }
-
 
         // get the actual factory
         $factory = $serviceSpecification->getFactory();
-
-        // merge service defined and runtime params
-        $params = clone Collection::cast([$serviceId, $this->getServicesFactory()] + $params);
-        $params = $params->values()->toArray();
 
         // substitute params with referenced services
         if ($this->getServicesFactory()->hasConfig()) {
             $params = $this->getServicesFactory()->getConfig()->processParameters($params);
         }
 
+        // merge service defined and runtime params
+        $params = clone Collection::cast([$serviceId, $this->getServicesFactory()] + $params);
+        $params = $params->values()->toArray();
+
         $service = $factory(...$params);
 
         return $service;
     }
-
 }
