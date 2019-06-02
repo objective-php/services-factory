bfa123aaba8d78d561738d725f8e0c6a25552e56
Gauthier Delamarre <gdelamarre@gmail.com>
2019-06-02 17:05:52 +0200

    Start working on documentation

--- docs/index.md
+++ docs/index.md
@@ -4,4 +4,14 @@ objective-php/services-factory
 Description
 ------------
 
-Please describe here the purpose of the package
+ServicesFactory is a dependency container. It implements ```Psr\Container\ContainerInterface```
+
+Starting with version 3.0, it also implements an autowiring mechanism to resolve automatically dependencies expected by the constructor.
+ 
+
+Documentation
+-------------
+
+   - [Quick Start](01.quick-start.md)
+   - [Detailed usage](02.detailed-usage.md)
+   - [Config directives reference](03.config-directives.md)
--- src/Config/ServiceDefinition.php
+++ src/Config/ServiceDefinition.php
@@ -31,12 +31,18 @@ class ServiceDefinition extends AbstractMultiComplexDirective implements IgnoreD
 
 
     /**
+     * Service reference
+     *
+     * This id can be used to override the default reference. This is usually neither necessary to fill it, nor a good
+     * practice.
+     *
      * @config-attribute
-     * @config-example-value 'service.id' // if present, overrides the key
+     * @config-example-value "my.service.id"
      * @var string
      *
      */
     protected $id;
+
 
     /**
      * Service class name
@@ -44,33 +50,57 @@ class ServiceDefinition extends AbstractMultiComplexDirective implements IgnoreD
      * Define here the FQCN of the class to instantiate to build the service.
      *
      * @config-attribute
-     * @config-example-value 'Fully\\Qualified\\Class\\Name'
+     * @config-example-value "Fully\\Qualified\\Class\\Name"
      * @var string
      */
     protected $class;
 
     /**
+     * Constructor parameters
+     *
+     * Array of parameters to pass to the service constructor. If none provided, the ServiceFactory will attempt to autowire the class.
      *
      * @config-attribute
-     * @config-example-value '["constructor", "params"]'
+     * @config-example-value array("constructor", "params")
      * @var array
      */
     protected $params = [];
 
     /**
+     * Dependency injection using setters
+     *
+     * You can provide the ServicesFactory with an array of setters to be called after service instantiation
+     *
      * @config-attribute
-     * @config-example-value '{"setDependency":"service(dependency.id)"}'
+     * @config-example-value {"setDependency":"service(dependency.id)"}
      * @var array
      */
     protected $setters = [];
 
+
     /**
+     * Set static flag
+     *
+     * Services are static by default, meaning that whe you get several time the same service, the very same object
+     * is returned. If you want ServicesFactory to build new instances each time you get the service, set the the
+     * static flag to false.
+     *
+     * @config-attribute
      * @var bool
      */
     protected $static = true;
 
     /**
-     * @var string[]
+     * Service aliases
+     *
+     * You can alias a service using any string. The most common use case is to
+     * alias a service using an interface name in order to make it available for
+     * autowiring
+     *
+     * @config-attribute
+     * @config-example-value array("Package\\ComponentInterface")
+     *
+     * @var array
      */
     protected $aliases = [];
 
